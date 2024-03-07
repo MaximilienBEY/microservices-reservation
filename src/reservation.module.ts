5b735797ff2a5fd5439110df48c7aaeb0c56a876
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-07 17:56:39 +0100

    Remove ConfigModule from MovieModule

--- src/reservation.module.ts
+++ src/reservation.module.ts
@@ -1,25 +1,12 @@
 import { CommonModule } from "@app/common"
 import { RmqModule } from "@app/common/rmq/rmq.module"
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import * as joi from "joi"
 
 import { ReservationController } from "./reservation.controller"
 import { ReservationService } from "./reservation.service"
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: joi.object({
-        DATABASE_URL: joi.string().required(),
-        RABBIT_MQ_URL: joi.string().required(),
-      }),
-      envFilePath: "./apps/reservation/.env",
-    }),
-    RmqModule,
-    CommonModule,
-  ],
+  imports: [RmqModule, CommonModule],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
