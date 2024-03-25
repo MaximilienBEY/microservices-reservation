c4b862f122977461bf2eb208a47386e19412d2d1
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-25 21:57:03 +0100

    make reservaztion

--- src/reservation.controller.ts
+++ src/reservation.controller.ts
@@ -1,7 +1,8 @@
-import { sleep } from "@app/common"
-import { Public } from "@app/common/auth/user.decorator"
+import { Public, User } from "@app/common/auth/user.decorator"
 import { RmqService } from "@app/common/rmq/rmq.service"
-import { Controller, Get } from "@nestjs/common"
+import { ReservationCreateType } from "@app/common/schemas/reservation/types"
+import { UserType } from "@app/common/schemas/user/types"
+import { Controller, Get, NotFoundException, Param, Post } from "@nestjs/common"
 import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices"
 import { ApiTags } from "@nestjs/swagger"
 import { HealthCheck, HealthCheckService } from "@nestjs/terminus"
@@ -17,6 +18,16 @@ export class ReservationController {
     private readonly health: HealthCheckService,
   ) {}
 
+  @Get(":id")
+  async getReservation(@Param("id") id: string, @User() user: UserType) {
+    return this.reservationService.getReservation(id, user)
+  }
+
+  @Post(":id/confirm")
+  async confirmReservation(@Param("id") id: string, @User() user: UserType) {
+    return this.reservationService.confirmReservation(id, user.uid)
+  }
+
   @Public()
   @Get("health")
   @HealthCheck()
@@ -26,19 +37,48 @@ export class ReservationController {
 
   // RMQ
   @EventPattern("reservation.create")
-  async reservationCreate(@Payload() data: any, @Ctx() context: RmqContext) {
+  async reservationCreate(
+    @Payload()
+    { userId, movieId, data }: { data: ReservationCreateType; userId: string; movieId: string },
+    @Ctx() context: RmqContext,
+  ) {
     const channel = context.getChannelRef()
     const originalMsg = context.getMessage()
     const replyTo = originalMsg.properties.replyTo
     const correlationId = originalMsg.properties.correlationId
 
-    console.log(Date.now(), "Reservation started")
-    await sleep(5000)
-    console.log(Date.now(), "Reservation created")
-
-    channel.sendToQueue(replyTo, Buffer.from(JSON.stringify({ reservationUid: "123" })), {
+    const response = await this.reservationService.makeReservation(movieId, userId, data)
+    channel.sendToQueue(replyTo, Buffer.from(JSON.stringify(response)), {
       correlationId,
     })
     this.rmqService.ack(context)
+  }
+
+  @EventPattern("reservation.movie.list")
+  async reservationByMovieList(
+    @Payload() { movieId }: { movieId: string },
+    @Ctx() context: RmqContext,
+  ) {
+    const reservations = await this.reservationService
+      .getReservationsByMovie(movieId)
+      .catch(() => null)
+    this.rmqService.ack(context)
+
+    if (!reservations) throw new NotFoundException("Movie not found")
+    return reservations
+  }
+
+  @EventPattern("reservation.sceance.list")
+  async reservationBySceanceList(
+    @Payload() { movieId, sceanceId }: { movieId: string; sceanceId: string },
+    @Ctx() context: RmqContext,
+  ) {
+    const reservations = await this.reservationService
+      .getReservationsBySceance(movieId, sceanceId)
+      .catch(() => null)
+    this.rmqService.ack(context)
+
+    if (!reservations) throw new NotFoundException("Movie not found")
+    return reservations
   }
 }
--- src/reservation.service.ts
+++ src/reservation.service.ts
@@ -1,7 +1,174 @@
-import { PrismaService } from "@app/common"
-import { Injectable } from "@nestjs/common"
+import { MailService, PrismaService } from "@app/common"
+import { ReservationCreateType, ReservationType } from "@app/common/schemas/reservation/types"
+import { UserType } from "@app/common/schemas/user/types"
+import { BadRequestException, HttpException, Injectable, NotFoundException } from "@nestjs/common"
 
 @Injectable()
 export class ReservationService {
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly expiredTimeout = new Map<string, NodeJS.Timeout>()
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mailService: MailService,
+  ) {}
+
+  private async expireReservation(reservationId: string, sceanceId: string) {
+    await this.prisma.reservation.update({
+      where: { uid: reservationId },
+      data: { status: "EXPIRED" },
+    })
+    this.expiredTimeout.delete(reservationId)
+    await this.checkStatus(sceanceId)
+  }
+  private async checkStatus(sceanceId: string) {
+    const sceance = await this.prisma.sceance.findFirst({
+      where: { uid: sceanceId },
+      include: { reservations: true },
+    })
+    // If there's an open reservation, we do nothing
+    if (sceance?.reservations.find(r => r.status === "OPEN")) return
+
+    // If there's a pending reservation, we open it
+    const pending = sceance?.reservations.find(r => r.status === "PENDING")
+    if (pending) {
+      await this.prisma.reservation.update({
+        where: { uid: pending.uid },
+        data: { status: "OPEN", expiresAt: new Date(Date.now() + 1 * 1000 * 60) },
+      })
+      this.expiredTimeout.set(
+        pending.uid,
+        setTimeout(() => this.expireReservation(pending.uid, sceanceId), 1 * 1000 * 60),
+      )
+      return
+    }
+  }
+  private async formatReservation(reservationId: string): Promise<ReservationType> {
+    const reservation = await this.prisma.reservation.findFirst({
+      where: { uid: reservationId },
+      include: { sceance: { include: { reservations: true } } },
+    })
+    if (!reservation) throw new NotFoundException("Reservation not found")
+
+    return {
+      uid: reservation.uid,
+      sceanceUid: reservation.sceanceUid,
+      status: reservation.status,
+      seats: reservation.nbSeats,
+      rank:
+        reservation.sceance.reservations
+          .filter(r => r.status === "PENDING")
+          .findIndex(r => r.uid === reservation.uid) + 1,
+      createdAt: reservation.createdAt,
+      updatedAt: reservation.updatedAt,
+      expiresAt: reservation.expiresAt,
+    }
+  }
+
+  async makeReservation(movieId: string, userId: string, data: ReservationCreateType) {
+    const sceance = await this.prisma.sceance.findFirst({
+      where: { movieUid: movieId, uid: data.sceance },
+      include: { room: true, reservations: true },
+    })
+    if (!sceance) return { type: "error", message: "Sceance not found" }
+
+    const seatsReserved = sceance.reservations
+      .filter(r => r.status !== "EXPIRED")
+      .reduce((acc, r) => acc + r.nbSeats, 0)
+    if (seatsReserved + data.nbSeats > sceance.room.seats)
+      return { type: "error", message: "Not enough seats, check late." }
+
+    const reservationId = await this.prisma.reservation
+      .create({
+        data: {
+          status: "PENDING",
+          nbSeats: data.nbSeats,
+          user: { connect: { uid: userId } },
+          sceance: { connect: { uid: sceance.uid } },
+        },
+      })
+      .then(r => r.uid)
+    await this.checkStatus(sceance.uid)
+
+    const reservation = await this.formatReservation(reservationId)
+    return { type: "success", data: reservation }
+  }
+  async confirmReservation(reservationId: string, userId: string) {
+    const user = await this.prisma.user.findFirst({ where: { uid: userId } })
+    const reservation = await this.prisma.reservation.findFirst({
+      where: { uid: reservationId, userUid: userId },
+      include: { sceance: { include: { movie: true } } },
+    })
+    if (!user) throw new NotFoundException("User not found")
+    if (!reservation) throw new NotFoundException("Reservation not found")
+    if (reservation.status === "CONFIRMED")
+      throw new BadRequestException("Reservation already confirmed")
+    else if (reservation.status === "EXPIRED") throw new HttpException("Reservation expired", 410)
+    else if (reservation.status === "PENDING") throw new BadRequestException("Reservation not open")
+
+    // If the server is restarted, we need to check if the reservation is expired
+    if (reservation.expiresAt && reservation.expiresAt < new Date()) {
+      await this.expireReservation(reservationId, reservation.sceanceUid)
+      throw new HttpException("Reservation expired", 410)
+    }
+
+    await this.prisma.reservation.update({
+      where: { uid: reservationId },
+      data: { status: "CONFIRMED", expiresAt: null },
+    })
+    clearTimeout(this.expiredTimeout.get(reservationId))
+    this.expiredTimeout.delete(reservationId)
+
+    await this.checkStatus(reservation.sceanceUid)
+
+    await this.mailService.sendEmail(
+      user.email,
+      "Reservation confirmed",
+      `
+<h1>Reservation confirmed</h1>
+<p>Your reservation for <b>${reservation.sceance.movie.name}</b> has been confirmed.</p>
+<p>Seats: <b>${reservation.nbSeats}</b></p>
+<p>Date: <b>${reservation.sceance.date.toISOString()}</b></p>
+<p>Thank you for using our service.</p>`,
+    )
+    return this.formatReservation(reservationId)
+  }
+  async getReservation(reservationId: string, user: UserType) {
+    const reservation = await this.prisma.reservation.findFirst({
+      where: { uid: reservationId },
+    })
+    if (!reservation || (user.role !== "ADMIN" && reservation.userUid !== user.uid))
+      throw new NotFoundException("Reservation not found")
+
+    return this.formatReservation(reservationId)
+  }
+
+  async getReservationsByMovie(movieId: string) {
+    const movie = await this.prisma.movie.findFirst({
+      where: { uid: movieId },
+      include: { sceances: true },
+    })
+    if (!movie) throw new NotFoundException("Movie not found")
+    return Promise.all(
+      movie.sceances.map(async sceance => this.getReservationsBySceance(movieId, sceance.uid)),
+    ).then(reservations => reservations.flat())
+  }
+  async getReservationsBySceance(movieId: string, sceanceId: string): Promise<ReservationType[]> {
+    const sceance = await this.prisma.sceance.findFirst({
+      where: { uid: sceanceId, movieUid: movieId },
+      include: { reservations: true },
+    })
+    if (!sceance) throw new NotFoundException("Sceance not found")
+
+    return sceance.reservations.map(r => ({
+      uid: r.uid,
+      sceanceUid: r.sceanceUid,
+      status: r.status,
+      seats: r.nbSeats,
+      rank:
+        sceance.reservations.filter(r => r.status === "PENDING").findIndex(r => r.uid === r.uid) +
+        1,
+      createdAt: r.createdAt,
+      updatedAt: r.updatedAt,
+      expiresAt: r.expiresAt,
+    }))
+  }
 }
